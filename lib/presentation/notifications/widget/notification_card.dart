ae7bb13383e02bb396517e0220cc1e052d32bdf3
Tulasi Reddy <tulasireddy.kagithala@karkinos.in>
2024-12-07 17:55:34 +0530

    Added inkWell Widget

--- lib/presentation/notifications/widget/notification_card.dart
+++ lib/presentation/notifications/widget/notification_card.dart
@@ -10,28 +10,31 @@ class NotificationTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      titleAlignment: ListTileTitleAlignment.top,
-      leading: ClipOval(
-        child: ColoredBox(
-          color: AppColors.rosePink,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.notifications, // Use different icons based on `notification['icon']`
-              color: notification['isNew'] ? Colors.blue : Colors.grey,
+    return InkWell(
+      onTap: (){},
+      child: ListTile(
+        titleAlignment: ListTileTitleAlignment.top,
+        leading: ClipOval(
+          child: ColoredBox(
+            color: AppColors.rosePink,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.notifications, // Use different icons based on `notification['icon']`
+                color: notification['isNew'] ? AppColors.vividOrange : AppColors.grey,
+              ),
             ),
           ),
         ),
-      ),
-      title: Text(notification['title'], style: context.textStyle?.titleLarge),
-      subtitle: Text(
-        notification['description'],
-        style: context.textStyle?.bodyMedium,
-      ),
-      trailing: Text(
-        DateFormat('hh:mm a').format(DateTime.parse(notification['timestamp'])),
-        style: context.textStyle?.titleSmall,
+        title: Text(notification['title'], style: context.textStyle?.titleLarge),
+        subtitle: Text(
+          notification['description'],
+          style: context.textStyle?.bodyMedium,
+        ),
+        trailing: Text(
+          DateFormat('hh:mm a').format(DateTime.parse(notification['timestamp'])),
+          style: context.textStyle?.titleSmall,
+        ),
       ),
     );
   }
